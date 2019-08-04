9c1eff57504ed7d310b486559867ad5714a6f878
Allen Kim <allenbkim@gmail.com>
2019-08-04 13:35:18 -0700

    Two spaces before class definition

--- scraper/run_script_scraper.py
+++ scraper/run_script_scraper.py
@@ -1,5 +1,6 @@
 from script_scraper import ScriptScraper
 
+
 def run_script_scraper(tv_scripts, script_letters, site_url, thread_count, download_directory):
   # Initialize the "Springfield, Springfield" screen scraper
   scraper = ScriptScraper(tv_scripts,
