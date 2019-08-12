2594ec5ce9671caec8d328594fa1b2eaa7258728
Allen Kim <allenbkim@gmail.com>
2019-08-12 16:54:12 -0700

    Implement TV file import process

--- scraper/pg_import_creator.py
+++ scraper/pg_import_creator.py
@@ -12,7 +12,9 @@ class PostgressImportCreator():
     self.tv_shows_per_file = tv_shows_per_file
     self.current_movie_file = None
     self.current_tv_file = None
-    self.log_file = 'postgressimportcreator_{time}.log'.format(time=strftime('%Y-%m-%d %H-%M'))
+    start_time = strftime('%Y-%m-%d %H-%M')
+    self.log_file = 'postgressimportcreator_{time}.log'.format(time=start_time)
+    self.start_time = start_time
 
     logging.basicConfig(filename=self.log_file, format='%(levelname)s: %(message)s', level=logging.DEBUG)
 
@@ -44,37 +46,87 @@ class PostgressImportCreator():
       try:
         movie_files = os.listdir('/'.join([self.movie_dir, letter]))
         for movie_file in movie_files:
-          movie_title, movie_year = self.extract_movie_title_and_year_from_file_name(movie_file)
-          with open('/'.join([self.movie_dir, letter, movie_file])) as movie_file_contents:
-            movie_script = movie_file_contents.read()
-          
-          movie_data = '\t'.join([movie_title, movie_year, movie_script]) + '\n'
-          self.current_movie_file.write(movie_data)
-          movies_per_file_counter += 1
+          try:
+            movie_title, movie_year = self.extract_movie_title_and_year_from_file_name(movie_file)
+            with open('/'.join([self.movie_dir, letter, movie_file]), 'r', encoding='ISO-8859-1') as movie_file_contents:
+              movie_script = movie_file_contents.read()
+            
+            movie_data = '\t'.join([movie_title, movie_year, movie_script]) + '\n'
+            self.current_movie_file.write(movie_data)
+            movies_per_file_counter += 1
 
-          if movies_per_file_counter >= self.movies_per_file:
-            movie_file_count += 1
-            self.increment_movie_import_file(movie_file_count)
-            movies_per_file_counter = 0
+            if movies_per_file_counter >= self.movies_per_file:
+              movie_file_count += 1
+              self.increment_movie_import_file(movie_file_count)
+              movies_per_file_counter = 0
+          except Exception as e:
+            logging.error('Error occurred while processing movie ' + movie_file + ': ' + str(e))
       except Exception as e:
-        logging.error('Error occurred while processing ' + movie_file + ': ' + str(e))
+        logging.error('Error occurred while processing movie letter' + letter + ': ' + str(e))
+    
+    logging.info('Finished movie file processing')
   
   def create_tv_import_files(self):
     """Creates a series of TV data import files for postgres
     """
-    # for each letter in tv_dir
-    #   for each show in letter
-    #     store initial year
-    #     for each season
-    #       calculate season year
-    #       for each episode in season
-    #         up to tv_shows_per_file
-    #           tv title \t show \t season number \t season year \t script
+    logging.info('Starting TV file creation')
+    tv_file_count = 0
+    tv_per_file_counter = 0
 
-    print('sup')
+    self.increment_tv_import_file(tv_file_count)
+
+    tv_letters = os.listdir(self.tv_dir)
+    for letter in tv_letters:
+      try:
+        if os.path.isfile('/'.join([self.tv_dir, letter])):
+          continue
+
+        show_dirs = os.listdir('/'.join([self.tv_dir, letter]))
+        for show_dir in show_dirs:
+          try:
+            if os.path.isfile('/'.join([self.tv_dir, letter, show_dir])):
+              continue
+
+            show_title, show_year = self.extract_tv_title_and_year_from_dir(show_dir)
+            show_season_dirs = os.listdir('/'.join([self.tv_dir, letter, show_dir]))
+
+            current_show_year = show_year
+            for show_season_dir in show_season_dirs:
+              try:
+                if os.path.isfile('/'.join([self.tv_dir, letter, show_dir, show_season_dir])):
+                  continue
+      
+                show_season = show_season_dir.split(' ')[1]
+                show_season_eps = os.listdir('/'.join([self.tv_dir, letter, show_dir, show_season_dir]))
+                for show_season_ep in show_season_eps:
+                  try:
+                    ep_number, ep_title = self.extract_tv_episode_number_and_title_from_file(show_season_ep)
+                    with open('/'.join([self.tv_dir, letter, show_dir, show_season_dir, show_season_ep]), 'r', encoding='ISO-8859-1') as ep_contents:
+                      show_script = ep_contents.read()
+                    
+                    show_data = '\t'.join([show_title, show_year, show_season, current_show_year, ep_number, ep_title, show_script]) + '\n'
+                    self.current_tv_file.write(show_data)
+                    tv_per_file_counter += 1
+
+                    if tv_per_file_counter >= self.tv_shows_per_file:
+                      tv_file_count += 1
+                      self.increment_tv_import_file(tv_file_count)
+                      tv_per_file_counter = 0
+                  except Exception as e:
+                    logging.error('create_tv_import_files() - episode loop: Error occurred while processing TV show ' + show_title + ', season ' + show_season + ', episode ' + show_season_ep + ': ' + str(e))
+
+                current_show_year = str(int(current_show_year) + 1)
+              except Exception as e:
+                logging.error('create_tv_import_files() - season loop: Error occurred while processing TV show ' + show_title + ', season ' + show_season_dir + ': ' + str(e))
+          except Exception as e:
+            logging.error('create_tv_import_files() - show loop: Error occurred while processing TV show ' + show_dir + ': ' + str(e))
+      except Exception as e:
+        logging.error('create_tv_import_files() - letter loop: Error occurred while processing TV letter ' + letter + ': ' + str(e))
+
+    logging.info('Finished TV file processing')
   
   def extract_movie_title_and_year_from_file_name(self, file_name):
-    """Returns the year from a movie file name, e.g. 'Movie Title_1994.txt' -> '1994'
+    """Returns the title and year from a movie file name, e.g. 'Movie Title_1994.txt' -> 'Movie Title', '1994'
     """
     title = file_name[:]
     if title.lower().endswith('.txt'):
@@ -89,17 +141,35 @@ class PostgressImportCreator():
     else:
       raise ValueError('File name in unsupported format: ' + file_name)
   
+  def extract_tv_title_and_year_from_dir(self, show_dir):
+    """Returns the title and year from a TV show directory name, e.g. 'TV Show Title_1994' -> 'TV Show Title', '1994'
+    """
+    title = show_dir[:]
+    if title.find('_') > -1:
+      last_underscore = title.rfind('_')
+      return title[:last_underscore], title[last_underscore+1:]
+    else:
+      raise ValueError('TV show directory missing year information')
+  
+  def extract_tv_episode_number_and_title_from_file(self, file_name):
+    """Returns the TV show episode number and title from a TV show file name, e.g. '1. Episode Title.txt' -> '1', 'Episode Title'
+    """
+    title = file_name[:]
+    if title.lower().endswith('.txt'):
+      title = title[:-4]
+      separator = '. '
+      ep_separator = title.find(separator)
+      if ep_separator > -1:
+        return title[:ep_separator], title[ep_separator+len(separator):]
+    else:
+      raise ValueError('File name in unsupported format: ' + file_name)
+  
   def increment_movie_import_file(self, counter):
     if self.current_movie_file:
       self.current_movie_file.close()
-    self.current_movie_file = open('pg_import_movie_' + str(counter) + '.txt', 'w')
+    self.current_movie_file = open('pg_import_movie_' + self.start_time + '_' + str(counter) + '.txt', 'w')
   
   def increment_tv_import_file(self, counter):
     if self.current_tv_file:
       self.current_tv_file.close()
-    self.current_tv_file = open('pg_import_tv_' + str(counter) + '.txt')
-
-
-if __name__ == '__main__':
-  pg = PostgressImportCreator('/users/allen/documents/dev/script-database/scraper/test/tv', '/users/allen/documents/dev/script-database/scraper/test/movies')
-  pg.create_import_files()
+    self.current_tv_file = open('pg_import_tv_' + self.start_time + '_' + str(counter) + '.txt', 'w')
