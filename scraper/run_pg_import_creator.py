b026ff4230a2664cc00d9f1242563b5c5f18a012
Allen Kim <allenbkim@gmail.com>
2019-08-12 16:55:16 -0700

    Add Postgres import process runner

--- scraper/run_pg_import_creator.py
+++ scraper/run_pg_import_creator.py
@@ -2,8 +2,8 @@ import pg_import_creator
 
 
 def main():
-  tv_dir = '/users/allen/documents/dev/script-database/scraper/test/tv'
-  movie_dir = '/users/allen/documents/dev/script-database/scraper/test/movies'
+  tv_dir = ''
+  movie_dir = ''
   print('Starting import file creation')
   import_creator = pg_import_creator.PostgressImportCreator(tv_dir, movie_dir)
   import_creator.create_import_files()
