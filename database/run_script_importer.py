f4bf25cd2c2045cd94c3db4ab658af2443f97a52
Allen Kim <allenbkim@gmail.com>
2019-08-12 20:56:39 -0700

    Add error handling to runner

--- database/run_script_importer.py
+++ database/run_script_importer.py
@@ -2,10 +2,13 @@ from script_importer import ScriptImporter
 
 
 def run_script_importer(import_files_dir, db_info):
-  script_importer = ScriptImporter(import_files_dir, db_info)
-  print('Starting script import process')
-  script_importer.run_import_process()
-  print('Finished script import process')
+  try:
+    script_importer = ScriptImporter(import_files_dir, db_info)
+    print('Starting script import process')
+    script_importer.run_import_process()
+    print('Finished script import process')
+  except Exception e:
+    print('An error occurred during import process: {error}'.format(error=str(e)))
 
 
 if __name__ == '__main__':
