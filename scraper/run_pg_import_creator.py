f4cea94c295ae368e83c774a7322ddd8dde40b63
Allen Kim <allenbkim@gmail.com>
2019-08-12 20:58:36 -0700

    Add error handling to runner

--- scraper/run_pg_import_creator.py
+++ scraper/run_pg_import_creator.py
@@ -5,9 +5,13 @@ def main():
   tv_dir = ''
   movie_dir = ''
   print('Starting import file creation')
-  import_creator = pg_import_creator.PostgressImportCreator(tv_dir, movie_dir)
-  import_creator.create_import_files()
-  print('Import file creation finished. Please check log files.')
+
+  try:
+    import_creator = pg_import_creator.PostgressImportCreator(tv_dir, movie_dir)
+    import_creator.create_import_files()
+    print('Import file creation finished. Please check log files.')
+  except Exception as e:
+    print('An error occurred during the import file creation process: {error}'.format(error=str(e)))
 
 
 if __name__ == '__main__':
