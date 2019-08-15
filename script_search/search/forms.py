a2e45f65dc340e301901a04b271ca58421a0e91b
Allen Kim <allenbkim@gmail.com>
2019-08-15 16:35:35 -0700

    Add script type selector to SearchForm

--- script_search/search/forms.py
+++ script_search/search/forms.py
@@ -5,3 +5,4 @@ class SearchForm(forms.Form):
   search_terms = forms.CharField(min_length=1, max_length=200, strip=True, required=True)
   year_filter_low = forms.IntegerField(min_value=1900, max_value=2030, required=False)
   year_filter_high = forms.IntegerField(min_value=1900, max_value=2030, required=False)
+  script_type = forms.ChoiceField(choices=[('All', 'All'), ('M', 'Movie'), ('T', 'TV')], required=True)
