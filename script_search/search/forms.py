8c3a03fd5c1b323f3afd21ed11e911095fe10657
Allen Kim <allenbkim@gmail.com>
2019-08-20 16:29:48 -0700

    Update year and script type UI controls

--- script_search/search/forms.py
+++ script_search/search/forms.py
@@ -2,7 +2,12 @@ from django import forms
 
 
 class SearchForm(forms.Form):
+  year_choices = [('', '')]
+  year_choices += [(x, x) for x in range(1920, 2021)]
+
+  script_type_choices = [('All', 'All'), ('M', 'Movie'), ('T', 'TV')]
+
   search_terms = forms.CharField(min_length=1, max_length=200, strip=True, required=True, widget=forms.Textarea)
-  year_filter_low = forms.IntegerField(min_value=1900, max_value=2030, required=False)
-  year_filter_high = forms.IntegerField(min_value=1900, max_value=2030, required=False)
-  script_type = forms.ChoiceField(choices=[('All', 'All'), ('M', 'Movie'), ('T', 'TV')], required=True)
+  year_filter_low = forms.ChoiceField(choices=year_choices, required=False)
+  year_filter_high = forms.ChoiceField(choices=year_choices, required=False)
+  script_type = forms.ChoiceField(choices=script_type_choices, widget=forms.RadioSelect, required=True)
