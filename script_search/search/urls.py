df1dee7d5e8c403d74b6ff1a8f1b9b37ee8a1630
Allen Kim <allenbkim@gmail.com>
2019-08-20 17:22:26 -0700

    Have root page be login to the search page

--- script_search/search/urls.py
+++ script_search/search/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 app_name = 'search'
 urlpatterns = [
-  path('', views.index, name='index'),
+  path('', views.search, name='index'),
   path('searchscripts/', views.search, name='searchscripts'),
   path('<int:pk>/', views.ScriptDetailView.as_view(), name='detail'),
   path('logout/', views.logout_search, name='logout'),
