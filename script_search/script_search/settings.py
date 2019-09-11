3de7cb87241c296ecda1c2bd8806cc84e53849f2
Allen Kim <allenbkim@gmail.com>
2019-09-10 21:12:58 -0700

    Add default ALLOWED_HOSTS to settings.py

--- script_search/script_search/settings.py
+++ script_search/script_search/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = ''
 DEBUG = False
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.elasticbeanstalk.com', '.nlcscripts.org']
 
 
 # Application definition
