1b2107583499601d67c5948d6f63ded54e736e9e
Allen Kim <allenbkim@gmail.com>
2019-09-19 08:56:25 -0700

    Update Script admin to include search

--- web/script_search/search/admin.py
+++ web/script_search/search/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from .models import Script, ScriptMigration
 
 
+# Create ModelAdmin classes to customize interacting with models
+class ScriptAdmin(admin.ModelAdmin):
+  list_display = ('title', 'script_type', 'year', 'season', 'episode', 'episode_title')
+  search_fields = ['title', 'year', 'episode_title']
+  list_filter = ('script_type', 'year')
+
+
 # Register custom models to be able to view on admin site
-admin.site.register(Script)
+admin.site.register(Script, ScriptAdmin)
 admin.site.register(ScriptMigration)
